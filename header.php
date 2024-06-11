70221ececec2745867dcd69ee75e072d737b3eef
Gabeuuh <gabin.clavier22@gmail.com>
2024-06-11 17:44:12 +0200

    Début du projet, création des pages

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>La confiserie</title>
+</head>
+<body>
+    <header>
+        <nav>
+            
+
+        </nav>
+    </header>
+
+
+
+
+
+
