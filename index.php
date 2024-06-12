02a6f6f056c731bd21b3c083481331e93722080e
Gabeuuh <gabin.clavier22@gmail.com>
2024-06-12 11:05:24 +0200

    2ème div index

--- index.php
+++ index.php
@@ -6,7 +6,17 @@ include_once("header.php")
 <section class="promote">
 <div class="promote_left">
     <h1 class="promote_title">Bergamote de Nancy</h1>
-    <button class="promote_button">DECOUVRIR</button>
+    <button class="button">DECOUVRIR</button>
 </div>
 <img src="<?=SITEROOT?>assets/pictures/bergamote.png" alt="Bergamote de Nancy" class="promote_img">
+</section>
+
+<section class="artisanat">
+    <img src="<?=SITEROOT?>assets/pictures/artisant_nougat.jpg" alt="Image d'un de nos artisans au travail" class="img_artisant">
+    <div class="artisanat_right with-blur-backdrop">
+        <p class="text_artisant">Depuis 1847, notre maison prépare avec passion et savoir-faire toutes nos sucreries</p>
+        <button class="button">En savoir plus</button>
+    </div>
+
+
 </section>
