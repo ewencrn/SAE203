ea19a170a04091cafd5b642eccf6e440b09d93b4
ewencrn <ewencornou@orange.fr>
2024-06-13 09:24:44 +0200

    produit css

--- produit.php
+++ produit.php
@@ -1,13 +1,15 @@
 <?php include_once("header.php") ?>
+
 <body>
-    <div class="produit_bonbon">
-            <div class="hover_produit_bonbon">
-            <img src="<?=SITEROOT?>assets/pictures/berlingot.png" class="image_bonbon">
-            <p class="nom_bonbon">Berlingot </p>
-            <p class="stock_bonbon">En stock</p>
-        </div>
+    <div class="produit">
+    <div class="texte_produit">
+        <p class="nom_produit"> Bergamote de Nançy </p>
+        <p class="id"> Id : 165842 </p>
+        <p class="quantite"> Boîte de 300g </p>
+        <p class="prix"> 25,50€ </p>
+        <p class="par_kg"> (77,27€ par Kg) </p>
+        <p class="histoire"> Depuis 1840, les seules, véritables, et authentiques Bergamottes de  Nancy, parfumées à l'essence naturelle de bergamotte, emballées  séparément, et rangées dans une grande boîte métallique ronde. </p>
     </div>
-</body>
+    <img src="<?=SITEROOT?>assets/pictures/bergamote.png" class="img_produit">
 
-
-
+    </div>
