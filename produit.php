0f3849964ff731003354d080a1c778d1a564c517
Gabeuuh <gabin.clavier22@gmail.com>
2024-06-16 21:53:06 +0200

    Fin Gestion + page produit dynamique

--- produit.php
+++ produit.php
@@ -1,15 +1,21 @@
-<?php include_once("header.php") ?>
+<?php include_once("header.php");
+if($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $confiserie = get_confis_by_id($_GET['confiserie_id']);
+    print_r($confiserie); 
+}
+
+?>
 
 
 
     <div class="produit">
 <div class="produit_top">
     <div class="texte_produit">
-        <p class="nom_produit"> Bergamote de Nançy </p>
-        <p class="id"> Id : 165842 </p>
-        <p class="quantite"> Boîte de 300g </p>
-        <p class="prix"> 25,50€ </p>
-        <p class="par_kg"> (77,27€ par Kg) </p>  
+        <p class="nom_produit"> <?=$confiserie[0]["nom"] ?> </p>
+        <p class="id"> Id : <?=$confiserie[0]["id"]?> </p>
+        <p class="quantite"> <?=$confiserie[0]["type"]?> </p>
+        <p class="prix"> <?=$confiserie[0]["prix"]?> </p>
+        <p class="par_kg"> <?=$confiserie[0]["description"]?> </p>  
     </div>
 
     <img src="<?=SITEROOT?>assets/pictures/bergamote.png" class="img_produit">
